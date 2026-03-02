f00ff2f9341546fcfe5ab0fbb0f79853dbf79a24
valentinsld <valentin.salaud@gmail.com>
2026-03-02 15:55:56 +0100

    feat(nuxt.config.ts): config css + head + glsl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
+import glsl from "vite-plugin-glsl"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Mistral Model Use Case",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { name: "description", content: "A Nuxt 3 project" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
+
+  css: ["~/assets/styles/main.scss"],
+
+  modules: ["lenis/nuxt"],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/styles/variables/mixins.scss" as *;\n`,
+        },
+      },
+    },
+    plugins: [
+      glsl({
+        include: [
+          // Glob pattern, or array of glob patterns to import
+          "**/*.glsl",
+          "**/*.wgsl",
+          "**/*.vert",
+          "**/*.frag",
+          "**/*.vs",
+          "**/*.fs",
+        ],
+      }) as any,
+    ],
+  },
 })

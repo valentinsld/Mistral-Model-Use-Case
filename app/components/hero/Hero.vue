570791e2d05871b8ac0966f8e762a22e268a0a3a
valentinsld <valentin.salaud@gmail.com>
2026-03-02 16:49:38 +0100

    feat(components/Hero): init hero textes and init button animated

---
+++ app/components/hero/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hero">
+    <div class="hero__container">
+      <div class="container__left">
+        <h1 class="left__title">
+          It starts with frontier science.
+        </h1>
+
+        <p class="left__description">
+          State of the art models with cutting edge capabilities, from cloud to edge.
+        </p>
+
+        <UiButtonAnimated class="left__button">
+          Contact sales
+        </UiButtonAnimated>
+      </div>
+
+      <div class="container__right">
+        <!-- Right content goes here -->
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss" src="./Hero.scss" />
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,3 @@
 <template>
-  <div>
-    <h1>
-      Hello
-    </h1>
-  </div>
+  <Hero />
 </template>
